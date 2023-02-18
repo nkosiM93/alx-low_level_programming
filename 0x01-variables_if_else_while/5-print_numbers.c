cb71f185621ba75b9730c0f31c73eb6c742db0e4
AlphaCoder <mathiyathokozanin@gmail.com>
2023-02-18 04:03:49 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Print base 10 digits
+ *Description: Program prints all digits of the base ten system
+ *return: 0, ends the program
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number <= 9; number++)
+		printf("%d", number);
+	printf("\n");
+	return (0);
+}
