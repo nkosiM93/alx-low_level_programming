89b688d9fdcd600e1ed4d856ac4d8393706920d6
AlphaCoder <mathiyathokozanin@gmail.com>
2023-02-17 19:36:32 -0800

    1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - print ABC..
+ *Description: d
+ *Return: 0, end
+*/
+int main(void)
+{
+	char abc;
+
+	for (abc = 97; abc <= 122; abc++)
+	{
+		putchar(abc);
+	}
+	putchar('\n');
+	return (0);
+}
