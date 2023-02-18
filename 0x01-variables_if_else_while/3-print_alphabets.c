f8a9405c94682d6606696bd343b141f5572d3507
AlphaCoder <mathiyathokozanin@gmail.com>
2023-02-17 20:15:19 -0800

    corr

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,16 +6,16 @@
 */
 int main(void)
 {
-        char abc;
+	char abc;
 
-        for (abc = 97; abc <= 122; abc++)
-        {
-                putchar(abc);
-        }
+	for (abc = 97; abc <= 122; abc++)
+	{
+		putchar(abc);
+	}
 	for (abc = 65; abc <= 90; abc++)
-        {
-                putchar(abc);
-        }
-        putchar('\n');
-        return (0);
+	{
+		putchar(abc);
+	}
+	putchar('\n');
+	return (0);
 }
