75b0c753ca3f3f328e784f3c634d9a145b8c4828
AlphaCoder <mathiyathokozanin@gmail.com>
2023-02-21 09:12:43 -0800

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,6 @@
 
 int main(void)
 {
-	if (write(1, "and that piece of art is useful\" - Dora Korpar,"
-		  " 2015-10-19\n", 57) != 57)
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 57)
 		return (1);
-	else
-		return (0);
 }
