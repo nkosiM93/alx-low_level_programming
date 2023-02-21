b68f82b21bb3c5003934ee04772acc2fc87872ba
AlphaCoder <mathiyathokozanin@gmail.com>
2023-02-21 10:43:49 -0800

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ *main - prints ABC
+ *Description: Program prints alphabet in lowercase.
+ *
+ *Return: 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	for (char character = 'a'; character <= 'z'; character++)
+	{
+		write(1, &character, 1);
+	}
+}
+
+int main(void)
+{
+	print_alphabet();
+	write(1, "\n", 1);
+	return (0);
+}
