4e0c3a0f94691ca7e333b7fff8f06c198760af7e
AlphaCoder <mathiyathokozanin@gmail.com>
2023-02-21 11:58:17 -0800

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,8 +10,10 @@ void print_alphabet_x10(void)
 	int a;
 
 	for (a = 0; a < 10; a++)
+	{
 		for (index = 'a'; index <= 'z'; index++)
-		{
-		       write(1, &index, 1);
-		}
+			write(1, &index, 1);
+
+		write(1, "\n", 1);
+	}
 }
