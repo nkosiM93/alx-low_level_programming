035c4dc7d11d2632077e178fd46e569277a1398c
AlphaCoder <mathiyathokozanin@gmail.com>
2023-02-21 17:36:55 -0800

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,11 +17,11 @@ int print_sign(int n)
 	else if (n == 0)
 	{
 		write(1, "0", 1);
-                return(0);
+		return(0);
 	}
 	else
 	{
 		write(1, "-", 1);
-                return(-1);
+		return(-1);
 	}
 }
