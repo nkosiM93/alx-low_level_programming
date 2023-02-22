a792dca736cd3fed96d0faf289c062bb536b72f6
AlphaCoder <mathiyathokozanin@gmail.com>
2023-02-21 17:30:45 -0800

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *print_sign - prints value sign
+ *@n: value to be checked
+ *Return: 1 - positive, 0 - zero, -1 - negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		write(1, "+", 1);
+		return(1);
+	}
+	else if (n == 0)
+	{
+		write(1, "0", 1);
+                return(0);
+	}
+	else
+	{
+		write(1, "-", 1);
+                return(-1);
+	}
+}
