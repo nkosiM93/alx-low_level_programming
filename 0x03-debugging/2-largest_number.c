f5fdd288ca7331d1b8a55ebdc111730a45bb3902
AlphaCoder <mathiyathokozanin@gmail.com>
2023-02-23 01:46:23 -0800

    2. 0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,12 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
+    largest = a;
+    if (b > largest) {
+      largest = b;
     }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
+    if (C > largest) {
+      largest = c;
     }
 
     return (largest);
