1aec424278f0c9e1f97cde3b17b73e3774567401
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-02 00:22:54 -0800

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - combine two sentences
+ * @dest: the other text
+ * @src: The other text
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a, c;
+
+	a = 0;
+
+	while (dest[a] != '\0')
+		a++;
+
+	c = 0;
+
+	while (src[c] != '\0')
+	{
+		dest[c + a] = src[c];
+		c++;
+	}
+	return (dest);
+}
