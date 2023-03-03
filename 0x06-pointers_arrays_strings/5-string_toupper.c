e372e39734ebfd873cfe4abe1a7b1fe62a10637a
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-03 12:07:16 -0800

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,12 +2,12 @@
 #include <ctype.h>
 
 /**
- * _char_toupper -  to uppercase
+ * string_toupper -  to uppercase
  * @str: the string
  * Return: string capitalised
  */
 
-char *_char_toupper(char *str)
+char *string_toupper(char *str)
 {
 
 	while (*str)
