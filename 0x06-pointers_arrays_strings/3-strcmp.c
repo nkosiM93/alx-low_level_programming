096da437d69104e0845b925806ee200be690391b
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-03 06:06:49 -0800

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp -  compare two strings
+ * @s1: ...
+ * @s2: ...
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int sc1, sc2;
+
+	sc1 = 0;
+
+	while (s1[sc1] != '\0')
+		sc1++;
+
+	sc2 = 0;
+
+	while (s2[sc2] != '\0')
+		sc2++;
+
+	if (sc1 < sc2)
+		return (-15);
+	else if (sc1 > sc2)
+		return (15);
+	else
+		return (0);
+}
