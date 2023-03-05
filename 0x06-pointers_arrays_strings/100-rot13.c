809333517e24cb384c77be7afa7da8406457e88c
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-05 02:51:21 -0800

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * rot13 - encrypt
+ * @str: ...
+ * Return: ...
+ */
+
+char *rot13(char *str)
+{
+	char rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+	char alpha52[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+	int s, d;
+
+	for (s = 0; s != '\0'; s++) /*String iteration*/
+		for (d = 0; d < 52; d++)
+		{
+			if (str[s] == alpha52[d])
+			{
+				str[s] = rot13[d];
+				break;
+			}
+		}
+	return (str);
+}
