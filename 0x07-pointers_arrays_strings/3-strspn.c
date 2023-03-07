c11307f5c75e3c0af6fa67136df92176a75c083b
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-06 23:00:54 -0800

    3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - returns the length of the initial what what
+ * @s: ...
+ * @accept: the characters to find in the string s
+ * Return: length
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count, count1;
+	int r, k;
+	
+	r = 0;
+	count = 0;
+
+	for (; s[r] != '\0'; r++)
+	{
+		count1 = count;
+		k = 0;
+		while (accept[k] != '\0')
+		{
+			if (accept[k] == s[r])
+			{
+				count++;
+				break;
+			}
+			else
+				k++;
+		}
+
+		if (count - count1 == 0)
+			break;
+	}
+
+	return (count);
+}
