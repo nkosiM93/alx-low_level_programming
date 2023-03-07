f46b3fc8bab95e8b334c72b5b7c462a69b4d5403
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-06 18:31:58 -0800

    0. memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset - ...
+ * @s: ...
+ * @b: ...
+ * @n: ...
+ * Return: ...
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int g;
+
+	g = 0;
+
+	for (; g < n; g++)
+	{
+		s[g] = b;
+	}
+
+	return (s);
+}
