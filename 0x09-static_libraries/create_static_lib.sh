455ab1771aa64608c9d70358c3244510ed6db0a4
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-13 08:43:27 -0700

    1. Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-ar rcs liball.a 0-isupper.o 1-isdigit.o 2-strlen.o 3-strcmp.o 5-strstr.o 0-memset.o 1-memcpy.o 2-strncpy.o 3-strspn.o 6-abs.o 0-strcat.o   1-strncat.o  3-islower.o  4-isalpha.o  9-strcpy.o 100-atoi.o 2-strchr.o 3-puts.o 4-strpbrk.o _putchar.oii
+0-isupper.o  0-strcat.o  1-isdigit.o  1-strncat.o  2-strlen.o   3-islower.o  3-strcmp.o  4-isalpha.o  5-strstr.o  9-strcpy.o
+0-memset.o   100-atoi.o  1-memcpy.o   2-strchr.o   2-strncpy.o  3-puts.o     3-strspn.o  4-strpbrk.o  6-abs.o     _putchar.o
 
