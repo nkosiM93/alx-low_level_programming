ff46f1fbf4156057675a7a212b68da9be4262fa8
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-14 17:43:51 -0700

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,18 +9,17 @@
  */
 
 int **alloc_grid(int width, int height) {
-    int **grid;  // Declare pointer to pointer to 2D array
+    int **grid;  
     int i, j;
 
-    // Allocate memory for pointer to 2D array
+
     grid = (int **) malloc(height * sizeof(int *));
 
-    // Allocate memory for 2D array
+
     for (i = 0; i < height; i++) {
         grid[i] = (int *) malloc(width * sizeof(int));
     }
 
-    // Initialize 2D array to zero
     for (i = 0; i < height; i++) {
         for (j = 0; j < width; j++) {
             grid[i][j] = 0;
