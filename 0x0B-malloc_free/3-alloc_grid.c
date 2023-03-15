267c6c6cf9b27b89f599dcdfa82bd7f38a9142b8
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-14 18:00:56 -0700

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,10 @@ int **alloc_grid(int width, int height) {
     grid = (int **) malloc(height * sizeof(int *));
 
     if (grid == NULL)
+    {
+	    free (grid);
 	    return (NULL);
+    }
 
     for (i = 0; i < height; i++) {
         grid[i] = (int *) malloc(width * sizeof(int));
