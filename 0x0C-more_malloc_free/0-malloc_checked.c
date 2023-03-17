60d9c099495accde777aef58c2511fd2065a45c2
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-17 13:43:45 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * malloc_checked - returns pointer and 98 if failed
+ * @b:...
+ * Return: void pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *pInt = malloc(b);
+
+	if (pInt == NULL)
+		exit(98);
+	else
+		return (pInt);
+}
