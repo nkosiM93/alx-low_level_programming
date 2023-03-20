d636b77b184766be2216ecb9b9f0a025321d3351
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-20 06:44:31 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -21,7 +21,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (returned == NULL)
 		return (NULL);
 	
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		*(returned + i) = 0;
 
 	return (returned);
