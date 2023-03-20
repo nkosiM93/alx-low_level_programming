ab646a33ab9cf426eff9734c53e7423a210ad207
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-20 03:40:15 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,20 +10,56 @@
  * Return: resultant string
  */
 
+char *str(char *theStr, char *s1, char *s2, int n)
+{
+	if (theStr == NULL)
+                return (NULL);
+
+	if (s1 == NULL)
+	{
+		strncpy(theStr, s2, n);
+		return (theStr);
+	}
+	else if (s2 == NULL)
+	{
+		strncpy(theStr, s1, n);
+		return (theStr);
+	}
+	else
+	{
+		strcpy(theStr, s1);
+		strncat(theStr, s2, n);
+		return (theStr);
+	}
+}
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *theStr;
 
-	if (strlen(s2) <= n)
-		theStr = malloc((strlen(s1) + sizeof(s2)));
-	else
-		theStr = malloc(strlen(s1) + n + 1);
-
-	if (theStr == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-
-	strcpy(theStr, s1);
-	strncat(theStr, s2, n);
-
-	return (theStr);
+	else if (s1 == NULL)
+	{
+		theStr = malloc(sizeof(s2));
+		return (str(theStr, s1, s2, n));
+	}
+	else if (s2 == NULL)
+	{
+		theStr = malloc(sizeof(s1));
+		return (str(theStr, s1, s2, n));
+	}
+	else
+	{
+		if (strlen(s2) <= n)
+		{
+			theStr = malloc((strlen(s1) + sizeof(s2)));
+			return (str(theStr, s1, s2, n));
+		}
+		else
+		{
+			theStr = malloc(strlen(s1) + n + 1);
+			return (str(theStr, s1, s2, n));
+		}
+	}
 }
