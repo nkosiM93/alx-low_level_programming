89646e770f27149fd2930951d3d5e5fbde4b7350
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-21 14:43:14 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct that describes a dog
+ * @age: dog's age
+ * @name: the name of the  dog
+ * @owner: the owner of the dog
+ *
+ * Description: ...
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
