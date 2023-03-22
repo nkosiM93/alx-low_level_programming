420995b20833b5d7eae410cf1e1ee42862208757
AlphaCoder <mathiyathokozanin@gmail.com>
2023-03-22 01:54:29 -0700

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,16 +11,16 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *my_dog = malloc(sizeof(dog_t));
-
 	char *_name = name;
 	char *_owner = owner;
 
+	dog_t *my_dog = malloc(sizeof(dog_t));
+
 	if (my_dog != NULL)
 	{
-		my_dog.name = _name;
-		my_dog.age = age;
-		my_dog.owner = _owner;
+		my_dog->name = _name;
+		my_dog->age = age;
+		my_dog->owner = _owner;
 	}
 
 	return (my_dog);
