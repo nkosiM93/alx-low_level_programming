e7943a207548af00190993f0874466332cd550e9
nkosiM93 <mathiyathokozanin@gmail.com>
2023-04-04 04:09:14 -0700

    Corrections

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,7 @@
-#include <stdlib.h>
-#include <stddef.h>
-
 #ifndef FUNC_POINTERS
 #define FUNC_POINTERS
-
+#include <stdlib.h>
+#include <stddef.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(char c);
