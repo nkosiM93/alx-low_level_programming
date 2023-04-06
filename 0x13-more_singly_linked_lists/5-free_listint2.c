7ca17691e8d87a6d057195434e117b338b39e634
nkosiM93 <mathiyathokozanin@gmail.com>
2023-04-06 03:36:53 -0700

    5. Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (!(*head))
+		return;
+
 	while (*head != NULL)
 	{
 		tmp = *head;
 		*head = (*head)->next;
 		 free(tmp);
 	}
-
-	if (!(*head))
-		return;
 }
