cbc4d46ac8c02dc663c439e3f3b74f941f4d0cb3
nkosiM93 <mathiyathokozanin@gmail.com>
2023-04-10 07:17:20 -0700

    3. 11

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * set_bit - sets a bit at a certain position
+ * clea_bit - sets a bit at a certain position
  * @n: the ineteger containing the bit to be set
  * @index: the positio of the bit to be set
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	int i;
 
@@ -15,7 +15,5 @@ int set_bit(unsigned long int *n, unsigned int index)
 
 	i = 1 << index;
 
-	*n |= i;
-
-	return (1);
+	return (*n |= i);
 }
