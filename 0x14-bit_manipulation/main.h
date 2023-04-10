2aeb8d2ae212d38f49685a959605fc9df9a6aa16
nkosiM93 <mathiyathokozanin@gmail.com>
2023-04-09 23:41:12 -0700

    0. 0

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+unsigned int _pow(unsigned int a, unsigned int d);
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 
 #endif
