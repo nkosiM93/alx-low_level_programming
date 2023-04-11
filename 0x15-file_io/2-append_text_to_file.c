856104cd450938ece93fb430006c5dd222fb6868
nkosiM93 <mathiyathokozanin@gmail.com>
2023-04-11 15:10:51 -0700

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,7 +4,7 @@
 /**
  * append_text_to_file - append tect to a text file
  * @filename: the name of the file to append text to
- * @text-content: the content to be appeded to the file
+ * @text_content: the content to be appeded to the file
  * Return: returns 1 if successful and0 if not
  */
 
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	fd = open(filename, O_APPEND | O_WRONLY);
 	if (fd == -1)
 		return (-1);
-	
+
 	if (text_content != NULL)
 	{
 		wrt = write(fd, text_content, strlen(text_content));
