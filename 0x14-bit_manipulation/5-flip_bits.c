8765d359e04e6fa87bbe93bb1856ad8df8d0c614
CodeAlpha <mathiyathokozanin@gmail.com>
2023-05-05 12:57:15 -0700

    In 3...2...1...Go!

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -2,16 +2,16 @@
 
 /**
  * flip_bits - returns the number of bits that need to be flipped
- * for one number ot the same as the another 
+ * for one number ot the same as the another
  * @n: the number to target
  * @m: the number to compare with the target
- * Return: the number of bit you would needd to flip to match 
- * another number 
+ * Return: the number of bit you would needd to flip to match
+ * another number
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int cnt = 0, i = 0; 
+	int cnt = 0, i = 0;
 	unsigned int mask = 1;
 
 	if (m == n)
@@ -19,7 +19,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 
 	for (; i < 32; i++)
 	{
-		if (((n | mask) == n && (m | mask) == m) || 
+		if (((n | mask) == n && (m | mask) == m) ||
 				((n | mask) != n && (m | mask) != m))
 		{
 			mask <<= 1;
