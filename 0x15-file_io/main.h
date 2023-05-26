75d1f6338f0140ee69214542d472048eeedef914
CodeAlpha <mathiyathokozanin@gmail.com>
2023-05-26 09:38:02 -0700

    4. elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,4 +10,13 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _copy(const char *filename, const char *fl2);
+void _checkelf(char *h);
+void _magic(char *h);
+void _class(char *h, int x64);
+void _data(char *h);
+void _version(char *h);
+void _os(char *h);
+void _abiv(char *h);
+void _type(char *h, int x64);
+void _entry(char *h, int x64);
 #endif
