856f7108afb7ecfda387cc9294886ae71991ef74
CodeAlpha <mathiyathokozanin@gmail.com>
2023-05-27 00:49:15 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,35 +1,41 @@
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
- * read_textfile - reads a certain number of characters and prints
- * them on the std output
- * @filename: the file to be read for the text
- * @letters: the number of letters to read from the text feile
- * Return: ...
+ * create_file - creates a file
+ *
+ * @filename: name of the file
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: Returns: 1 on success, -1 on failure
  */
-
-ssize_t read_textfile(const char *filename, size_t letters)
+int create_file(const char *filename, char *text_content)
 {
-	ssize_t count = 0;
-	FILE *fp;
-	char c;
+	int fd, checkw, l = 0;
 
-	if (filename != NULL)
+	if (filename == 0)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
 	{
-		fp = fopen(filename, "r");
-		if (fp == NULL)
-			return (0);
+		while (text_content[l] != 0)
+			l++;
+		checkw = write(fd, text_content, l);
 
-		while (letters > 0 && (c = getc(fp)) != EOF)
-		{
-			putc(c, stdout);
-			count++;
-			letters--;
-		}
-
-		fclose(fp);
-		return (count);
+		if (checkw == -1)
+			return (-1);
 	}
-	else
-		return (0);
+
+	close(fd);
+	return (1);
 }
+
