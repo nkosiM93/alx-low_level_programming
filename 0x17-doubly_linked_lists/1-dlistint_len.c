cc44f312f314fa89a7fa2a10732348cdc8a389ea
CodeAlpha <mathiyathokozanin@gmail.com>
2023-06-15 15:49:38 -0700

    Q1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in list
+ * @h: head pointer of the list
+ *
+ * Return: number of elements in the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)i
+{
+	const dlistint_t *dCopy;
+	size_t n = 0;
+
+	dCopy = h;
+
+	if (dCopy == NULL)
+		return (n);
+
+	while (dCopy != NULL)
+	{
+		dCopy = dCopy->next;
+		n++;
+	}
+
+	return (n);
+}
