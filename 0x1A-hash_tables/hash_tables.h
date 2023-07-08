5e4dff90f8605b8b012c98e8bf402b92443c6754
CodeAlpha <mathiyathokozanin@gmail.com>
2023-07-08 08:46:22 -0700

    Create a hash table (Q0: 0. >>> ht = {})

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -7,8 +7,6 @@
 
 #define SIZE 50
 
-hash_table_t *hash_table_create(unsigned long int size);
-
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -37,5 +35,7 @@ typedef struct hash_table_s
      unsigned long int size;
      hash_node_t **array;
 } hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
 
 #endif
