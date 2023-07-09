4691472c756d8acea65fc4388ec580a22647148f
CodeAlpha <mathiyathokozanin@gmail.com>
2023-07-09 08:26:23 -0700

    Q3 correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,33 +12,74 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *node, *element, *temp;
+	char *str;
+
+	if (!ht || !key || !value)
+		return (0);
+
+	node = createNode(key, value);
+	if (!node)
+		return (0);
+	element = ht->array[index]; /*get the head of the list on index*/
+	if (!element)/*Add the new element as head of new list*/
+	{
+		ht->array[index] = node;
+		return (1);
+	}
+	if (strcmp((const char *)element->key, key) == 0
+			&& strcmp((const char *)element->value, value) != 0)
+	{
+		str = element->value;
+		free(element->value);
+		element->value = strdup(node->value);
+		if (element->value == NULL)
+		{
+			free(node);
+			element->value = str;
+			return (0);
+		}
+		free(node);
+		return (1);
+	}
+	else if (strcmp((const char *)element->key, key) != 0)
+        {
+		temp = element;
+		node->next = temp;
+		ht->array[index] = node;
+		return (1);
+        }
+	else
+		free(node);
+	return (0);
+}
+
+/**
+ * createNode - creates a list node (key-value pair)
+ * @key: The key
+ * @value: The value corresponding to the key
+ * Return: returns a newly-created node
+ */
+
+hash_node_t *createNode(const char *key, const char *value)
+{
+	hash_node_t *node;
 
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
-		return (0);
+		return (NULL);
 	node->key = strdup(key);
 	if (!node->key)
 	{
 		free(node);
-		return (0);
+		return (NULL);
 	}
-	node->value =strdup(value);
+	node->value = strdup(value);
 	if (!node->value)
 	{
+		free(node->key);
 		free(node);
-		return (0);
+		return (NULL);
 	}
-	element = ht->array[index];
-	if (!element)
-	{
-		node->next = NULL;
-		element = node;
-		ht->array[index] = element;
-		return (1);
-	}
-	temp = element;
-	node->next = temp;
-	element = node;
-	ht->array[index] = element;
-	return(1);
+	node->next = NULL;
+	return (node);
 }
