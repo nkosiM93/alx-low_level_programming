0f39d252124a77b8991f86957cc971414846910c
CodeAlpha <mathiyathokozanin@gmail.com>
2023-07-09 14:29:49 -0700

    Q4. Crushing it

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,16 +12,13 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index =
 		key_index((const unsigned char *)key, ht->size);
 	hash_node_t *node;
-	char *key2 = strdup(key);
 
 	if (!ht || !key)
-		return (NULL);
-	if (!key2)
 		return (NULL);
 	node = ht->array[index];
 	while (node)
 	{
-		if (strcmp(node->key, key2) == 0)
+		if (strcmp(node->key, key) == 0)
 			return (node->value);
 		node = node->next;
 	}
